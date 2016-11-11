c2b101cfad8f136eb87bb99dc28a763570c3227f
muniv <dmtgjh@naver.com>
2016-11-11 13:39:17 +0900

    빠띠 홈 상단의 스타일을 바꿉니다 #593

--- src/models/parti.ts
+++ src/models/parti.ts
@@ -3,9 +3,15 @@ import { Group } from '../models/group';
 export interface Parti {
   id: number;
   title: string;
+  body: string;
   slug: string;
   logo_url: string;
   group: Group;
+  updated_at: string;
+  latest_members_count: number;
+  latest_posts_count: number;
+  members_count: number;
+  posts_count: number;
   is_member: boolean;
   is_made_by: boolean;
 }
