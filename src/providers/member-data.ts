39bc6bd04edf3f5ca1dec1ad5bac90e3f38bb177
qus <qus1225@gmail.com>
2016-11-23 16:28:12 +0900

    그룹이 속한 빠띠의 가입, 탈퇴, 멤버조회가 됩니다

--- src/providers/member-data.ts
+++ src/providers/member-data.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
+import { RequestOptions, URLSearchParams } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 import { ApiHttp } from '../providers/api-http'
@@ -13,17 +14,41 @@ export class MemberData {
   ) {}
 
   join(parti: Parti): Observable<void> {
-    return this.http.post(`/api/v1/parties/${parti.slug}/members`)
+    let requestOptions = new RequestOptions();
+
+    if(!!parti.group) {
+      let searchParams = new URLSearchParams();
+      searchParams.set('group_slug', String(parti.group.slug));
+      requestOptions.search = searchParams;
+    }
+
+    return this.http.post(`/api/v1/parties/${parti.slug}/members`, requestOptions)
       .map(res => {});
   }
 
   cancel(parti: Parti): Observable<void> {
-    return this.http.delete(`/api/v1/parties/${parti.slug}/members`)
+    let requestOptions = new RequestOptions();
+
+    if(!!parti.group) {
+      let searchParams = new URLSearchParams();
+      searchParams.set('group_slug', String(parti.group.slug));
+      requestOptions.search = searchParams;
+    }
+
+    return this.http.delete(`/api/v1/parties/${parti.slug}/members`, requestOptions)
       .map(res => {});
   }
 
   allOfParti(parti: Parti): Observable<Member[]> {
-    return this.http.get(`/api/v1/parties/${parti.slug}/members`)
+    let requestOptions = new RequestOptions();
+
+    if(!!parti.group) {
+      let searchParams = new URLSearchParams();
+      searchParams.set('group_slug', String(parti.group.slug));
+      requestOptions.search = searchParams;
+    }
+
+    return this.http.get(`/api/v1/parties/${parti.slug}/members`, requestOptions)
       .map(res => <Member[]>(res.json().members));
   }
 }
